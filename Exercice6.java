bbc395c029ac19c503a21610e4577288d404352a
Karin Touchie <ktouchie@gmail.com>
2015-12-14 11:10:09 +0100

    Compile

--- Exercice6.java
+++ Exercice6.java
@@ -6,7 +6,7 @@ class Exercice6 {
         int a[] = {-2,-1,0,1,2,3,4,5,6,7,8};
         int length = a.length;
         
-        for (i=0; i<(a.length/2); i++) {
+        for (int i=0; i<(a.length/2); i++) {
             int x = a[i];
             a[i] = a[a.length-(i+1)];
             a[a.length- (i+1)] = x;
